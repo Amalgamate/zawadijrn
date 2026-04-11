cb76f30803564c1afc999aff26d5c1db667dd74a
Zawadi SMS <admin@zawadisms.co.ke>
2026-04-11 08:46:24 +0300

    chore: add migration to create missing database columns
    - Add primaryColor to schools table
    - Add shifNumber to users table
    - These columns exist in Prisma schema but weren't created via migrations
    - Will be applied during next deployment via 'prisma migrate deploy'

---
+++ server/prisma/migrations/20260411100000_add_missing_columns/migration.sql
@@ -0,0 +1,5 @@
+-- Add primaryColor column to schools table
+ALTER TABLE "schools" ADD COLUMN "primaryColor" TEXT;
+
+-- Add shifNumber column to users table  
+ALTER TABLE "users" ADD COLUMN "shifNumber" TEXT;
