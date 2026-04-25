5c099adb5d60d6d0dc580b1ab73a06d6d5ce9956
Zawadi SMS <admin@zawadisms.co.ke>
2026-04-26 02:07:18 +0300

    feat(dev): add @vitejs/plugin-basic-ssl for automatic local HTTPS certificates

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
   base: '/',
@@ -8,6 +9,7 @@ export default defineConfig({
   plugins: [
     react(),
     svgr(),
+    basicSsl(),
   ],
 
   resolve: {
